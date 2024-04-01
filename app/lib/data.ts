7b88c9204bea59ed6d7d95877aa30ffe934bd731
August <64938228+Augustbr01@users.noreply.github.com>
2024-03-31 22:25:42 -0400

    Update data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,34 +1,32 @@
 import { sql } from '@vercel/postgres';
 import { MessageTable } from './definitions'; // Importando o tipo MessageTable
 
-export async function sendMessage(text: string) {
+export async function sendMessageToDatabase(text: string): Promise<void> {
   try {
-    // Insere a mensagem na tabela de mensagens, não incluindo o campo id
     await sql<MessageTable>`INSERT INTO messages (text) VALUES (${text})`;
+    console.log('Message sent successfully!');
   } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to send the message.');
+    console.error('Error sending message:', error);
+    throw new Error('Failed to send message.');
   }
 }
 
-export async function fetchMessages() {
+export async function fetchMessagesFromDatabase(): Promise<MessageTable[]> {
   try {
-    // Retorna todas as mensagens da tabela de mensagens
     const data = await sql<MessageTable>`SELECT * FROM messages ORDER BY id`;
     return data.rows;
   } catch (error) {
-    console.error('Database Error:', error);
+    console.error('Error fetching messages:', error);
     throw new Error('Failed to fetch messages.');
   }
 }
 
-export async function fetchMessageById(id: string) {
+export async function fetchMessageById(id: string): Promise<MessageTable> {
   try {
-    // Retorna uma mensagem específica baseada no ID
     const data = await sql<MessageTable>`SELECT * FROM messages WHERE id = ${id}`;
     return data.rows[0];
   } catch (error) {
-    console.error('Database Error:', error);
+    console.error('Error fetching message:', error);
     throw new Error('Failed to fetch message.');
   }
 }
