fcf6f2fb53d2f89e6f1dcbed9ef57d6521f62b5d
August <64938228+Augustbr01@users.noreply.github.com>
2024-03-31 22:23:44 -0400

    Update definitions.ts

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,7 +2,7 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity of teaching, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
-export type Messages = {
+export type Message = {
   id: number;
   text: string;
   timestamp: string; // Você pode querer utilizar um tipo de data/hora mais específico aqui, dependendo do seu banco de dados e da linguagem de programação que está utilizando.
